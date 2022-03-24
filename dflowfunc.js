34dc6d8da9636987cb727aabb0fedfc43c247b57
Juan Carlos <juanca.alvarez.viedma@gmail.com>
2022-03-24 19:17:42 +0100

    ww

--- dflowfunc.js
+++ dflowfunc.js
@@ -102,20 +102,8 @@ module.exports = {
 
 setIdSession: function setIdSession(value){
     sessionId = value
-},
+}
 
-listBuckets: async function listBuckets() {
-    try {
-      const [buckets] = await storage.getBuckets();
-  
-      console.log('Buckets:');
-      buckets.forEach(bucket => {
-        console.log(bucket.name);
-      });
-    } catch (err) {
-      console.error('ERROR:', err);
-    }
-  }
 }
 
 
