7adefda57486467c222d8b8e2e3197d1f57f9d58
Juan Carlos <juanca.alvarez.viedma@gmail.com>
2022-03-24 11:24:19 +0100

    Add GOauth

--- index.js
+++ index.js
@@ -1,24 +1,54 @@
 const express = require("express");
 const axios = require('axios');
 const bodyParser = require('body-parser')
+const passport = require('passport');
+const cookieSession = require('cookie-session')
 const https = require('https'); // or 'https' for https:// URLs
 const fs = require('fs');
 const { param } = require("express/lib/request");
-
+require('./passport-setup');
 
 
 const app = express();
 
-app.use(bodyParser.urlencoded());
-app.use(bodyParser.json())
 
-app.listen(process.env.PORT, () => {
+// Auth middleware that checks if the user is logged in
+const isLoggedIn = (req, res, next) => {
+    if (req.user) {
+        next();
+    } else {
+        res.sendStatus(401);
+    }
+}
+
+
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.listen(proces.env.PORT, () => {
  console.log("El servidor está inicializado en el puerto " + process.env.PORT);
 });
 
 app.get('/', function (req, res) {  
     res.send("El servidor está inicializado en el puerto " + process.env.PORT);
 });
+
+// In this route you can see that if the user is logged in u can acess his info in: req.user
+app.get('/good', isLoggedIn, (req, res) => res.send(`Welcome mr ${req.user.displayName}!`))
+
+
+// Auth Routes
+app.get('/google', passport.authenticate('google', { scope: ["https://www.googleapis.com/auth/dialogflow"] }));
+
+app.get('/google/callback', passport.authenticate('google', { failureRedirect: '/failed' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/good');
+  }
+);
 
 app.post('/', function (req, res) {
     logRequest(req);    
@@ -58,7 +88,7 @@ app.post('/', function (req, res) {
         axios({
             method: 'post',
             url: "https://dialogflow.googleapis.com/v2/projects/calm-sylph-344317/agent/sessions/"+chatId+":detectIntent",
-            headers: {'Authorization': "key=" + "AIzaSyAJimM1CjjLjoi-aNulU34bu80RklaOrcc",'Content-Type': 'application/json'}, 
+            headers: {'Content-Type': 'application/json'}, 
             data: {
               "query_input" : {
                   "text": {
